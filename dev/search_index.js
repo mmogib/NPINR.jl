e0227e09ce613d17b389e2c62dfce1b0b0521800
Documenter.jl <documenter@juliadocs.github.io>
2024-10-20 18:10:29 +0000

    build based on de9f8e9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NPINR","category":"page"},{"location":"#NPINR","page":"Home","title":"NPINR","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NPINR.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NPINR]","category":"page"}]
+}
